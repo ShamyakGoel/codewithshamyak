5bced6c511b6f6a88707460a2b4ad507169a9aba
ShamyakGoel <sj907822@gmail.com>
2022-05-25 16:01:57 +0530

    Source code for videos

---
+++ code.js
@@ -0,0 +1,25 @@
+async function render(){
+    let url = "./playlists.json"
+    let res = await fetch(url)
+    let json = await res.json()
+    for(i in json){
+        let div = document.getElementById("playlists")
+        div.innerHTML += `
+        <div class="flex justify-center mx-3 my-3">
+  <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+  
+  <a href="./playlists.html?slug=${json[i].slug}">
+  <img class="rounded-t-lg" src="${json[i].image}" alt=""/>
+</a>
+
+    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Codewithshamyak</h5>
+    <p class="text-gray-700 text-base mb-4">
+      ${json[i].name}
+    </p>
+    <a href="./playlists.html?slug=${json[i].slug}"<button type="button" class="cursor-pointer inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View Videos</button></a>
+  </div>
+</div>
+        `
+    }
+}
+render()
--- main.js
+++ main.js
@@ -1,14 +1,61 @@
-function go(link , openInNewTab){
-    if(openInNewTab){
-        window.open(link, '_blank');
-    }
-    else{
-        location.replace(link);
+async function render(){
+    let url = "./data.json"
+    let res = await fetch(url)
+    let json = await res.json()
+    let cat = location.href.split("?")[1].split("=")[1]
+    let type;
+    for(i in json.data){
+        if(json.data[i].category !== cat){
+          console.log("no")
+          continue
+        }
+        console.log(json.data[i])
+        let code = json.data[i].code
+        let arr = Array.from(code)
+        for (let i = 0; i < arr.length; i++) {
+            if(arr[i] === '<'){
+                arr[i] = '&lt;'
+            }           
+            else if(arr[i] === '>'){
+                arr[i] = '&gt;'
+            }           
+        }
+        code = arr.join("")
+        code = code.replace('>', '&gt;', json.data[i].code.length)
+        let div = document.getElementById("codediv")
+        if(json.data[i].category === 'html'){
+          type = 'html'
+        }
+        div.innerHTML += `
+        <div class="flex justify-center mx-3 my-3">
+  <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+  ${json.data[i].embed}
+    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">Card title</h5>
+    <p class="text-gray-700 text-base mb-4">
+      ${json.data[i].name}
+    </p>
+    <button type="button" onclick={location.replace("./sourcecode.html?slug=${json.data[i].slug}")} class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Download Source Code</button>
+  </div>
+</div>
+        `
     }
 }
-function copy(id){
-    text = document.getElementById(id).innerText
-    console.log(text)
-    navigator.clipboard.writeText(text);
-    alert("Copied To ClipBoard")
+render()
+async function copy(slug){
+    let url = "./data.json"
+    let res = await fetch(url)
+    let json = await res.json()
+    for(i in json.data){
+        if(json.data[i].slug === slug){
+            navigator.clipboard.writeText(json.data[i].code)
+        }
+    }
 }
+// let url = "./data.json"
+// let res = await fetch(url)
+// let json = await res.json()
+// for(i in json.data){
+//     if(json.data[i].slug === slug){
+//       SaveBlobAs(json.data[i].code, slug+"."+type)
+//     }
+// }
